bea6729eb7ac9a4ee7725bc49abd10e336130e6e
huangke <huangke0802@163.com>
2018-04-18 19:45:52 +0800

    实现子路由的切换功能

--- myProject/src/main.js
+++ myProject/src/main.js
@@ -5,6 +5,10 @@ import IndexPage from "./pages/index"
 import mock from "./mockjs/mock"
 import VueResource from "vue-resource"
 import Detail from "./pages/detail.vue"
+import Count from "./pages/detail/count.vue";
+import Forecast from "./pages/detail/forecast.vue";
+import Analysis from "./pages/detail/analysis.vue";
+import Publish from "./pages/detail/publish.vue";
 
 //Vue.config.productionTip = false
 //使用路由router
@@ -12,16 +16,35 @@ Vue.use(VueRouter);
 Vue.use(VueResource);
 //实例化一个router
 let router = new VueRouter({
-  mode : "history",
-  routes : [{
-    path : "/",
-    component : IndexPage
-  },
-  {
-    path : "/detail",
-    component : Detail
-  }
-]
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      component: IndexPage
+    },
+    {
+      path: "/detail",
+      component: Detail,
+      children: [
+        {
+          path: "count",
+          component: Count
+        },
+        {
+          path: "forecast",
+          component: Forecast
+        },
+        {
+          path: "analysis",
+          component: Analysis
+        },
+        {
+          path: "publish",
+          component: Publish
+        }
+      ]
+    }
+  ]
 });
 
 new Vue({
