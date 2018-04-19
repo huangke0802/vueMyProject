f11519d66029730c86e286b2540024ff0676b893
huangke <huangke0802@163.com>
2018-04-19 23:21:36 +0800

    基础组件

---
+++ myProject/src/components/base/multiplyChooser.vue
@@ -0,0 +1,67 @@
+/* 多选的矩形选择 模块 */
+
+<template>
+  <div class="chooser-component">
+    <ul class="chooser-list">
+      <li v-for="(item, index) in selections"
+      @click="chooseSelction(index)"
+      :class="{active : nowIndex.indexOf(index) != -1}">{{item.label}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props : {
+    selections : {
+      type : Array,
+      default : [{
+        label : "test",
+        value : 0
+      }]
+    }
+  },
+  data(){
+    return {
+      nowIndex : [0],
+    }
+  },
+  methods : {
+    chooseSelction(index){
+      let n;
+      if(this.nowIndex.indexOf(index) === -1){
+          this.nowIndex.push(index);
+      }else{
+        n = this.nowIndex.indexOf(index);
+        this.nowIndex.splice(n, 1);
+      }
+      console.log(this.nowIndex);
+      this.$emit('on-change', this.nowIndex);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.chooser-component {
+  position: relative;
+  display: inline-block;
+}
+.chooser-list li{
+  display: inline-block;
+  border: 1px solid #e3e3e3;
+  height: 25px;
+  line-height: 25px;
+  padding: 0 8px;
+  margin-right: 5px;
+  border-radius: 3px;
+  text-align: center;
+  cursor: pointer;
+}
+.chooser-list li.active {
+  border-color: #4fc08d;
+  background: #4fc08d;
+  color: #fff;
+}
+</style>
+
